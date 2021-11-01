ede172630604b0647bd34f75f8e8774c7947b051
Jack <ixenroh@gmail.com>
2021-11-01 21:18:28 +0100

    Added player movement

--- Assets/Scripts/Player/PlayerShip.cs
+++ Assets/Scripts/Player/PlayerShip.cs
@@ -1,18 +1,18 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
     public const float startDelay = 1;
     public float turnRateDegreesSecond = 60;
+    public float maxTurnAngle = 90;
+    public float maxAngleSineSpeed = 1;
     
     private bool start = false;
     private bool turningUp = false;
     private float currentRotation = 0;
 
-    private void FixedUpdate()
+    private void AdjustAngle()
     {
         if(!start)
             return;
@@ -22,16 +22,29 @@ public class PlayerShip : MonoBehaviour
 
         var finalAngle = transform.eulerAngles + Vector3.forward * turnAngle;
 
-        if (Math.Abs(currentRotation + turnAngle) >= 90)
+        if (Math.Abs(currentRotation + turnAngle) >= maxTurnAngle)
         {
-            currentRotation = turnDir * 90;
+            currentRotation = turnDir * maxTurnAngle;
             finalAngle = Vector3.forward * currentRotation;
         }
         else
         {
             currentRotation += turnAngle;
         }
+        
         transform.eulerAngles = finalAngle;
+    }
+
+    private void AdjustPos()
+    {
+        var currentPos = transform.position;
+        currentPos.y += maxAngleSineSpeed * (currentRotation / maxTurnAngle);
+        transform.position = currentPos;
+    }
+    private void FixedUpdate()
+    {
+        AdjustAngle();
+        AdjustPos();
     }
 
     void Update()
@@ -45,5 +58,11 @@ public class PlayerShip : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
             turningUp = !turningUp;
 
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this);
+        Application.Quit();
     }
 }
