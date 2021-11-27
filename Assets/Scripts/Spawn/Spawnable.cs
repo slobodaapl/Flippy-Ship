d6d891c4c7543108dc1823abf0c573303ce6426c
Jack <ixenroh@gmail.com>
2021-11-27 18:18:54 +0100

    Testing

---
+++ Assets/Scripts/Spawn/Spawnable.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Spawnable<T> where T : Spawnable<T>
+{
+    public List<GameObject> Patterns;
+
+    private (float, float) XRange { get; set; }
+    private (float, float) YRange { get; set; }
+
+    private static readonly Lazy<T> Lazy =
+        new Lazy<T>(() => Activator.CreateInstance(typeof(T), true) as T);
+
+    public static T Instance => Lazy.Value;
+
+    public GameObject SpawnChoice(List<GameObject> filteredPrefabs)
+    {
+        return filteredPrefabs.PickRandom();
+    }
+
+    public void InitValues((float, float) xrange, (float, float) yrange, List<GameObject> patterns)
+    {
+        Patterns = patterns;
+        XRange = xrange;
+        YRange = yrange;
+    }
+    public abstract bool CheckConstraints();
+    public abstract List<GameObject> GetSpawnable(List<GameObject> prefabs);
+
+
+}
