f5abe97aa314c812d653eb4317a76cf796e0c097
Jack <ixenroh@gmail.com>
2021-12-01 03:00:15 +0100

    Fixed spawning bug

--- Assets/Scripts/Spawn/MineSpawnable.cs
+++ Assets/Scripts/Spawn/MineSpawnable.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 
 public sealed class MineSpawnable : Spawnable<MineSpawnable>
 {
-    private const int MaxMines = 5;
-    private int CurrentMines;
+    private const int maxMines = 5;
+    private int currentMines;
 
     public override bool CheckConstraints()
     {
-        return CurrentMines <= MaxMines;
+        return currentMines <= maxMines;
     }
 
     public override void SpawnInstantiate()
     {
-        var valid_patterns = patterns.Where(x => x.transform.childCount <= (MaxMines - CurrentMines)).ToList();
+        var valid_patterns = patterns.Where(x =>
+        {
+            var children = x.transform.childCount;
+            return (children == 0 ? 1 : children) <= (maxMines - currentMines);
+        }).ToList();
+        
+        if (valid_patterns.Count == 0) return;
+        
         var selected_pattern = SpawnChoice(valid_patterns);
+        var children = selected_pattern.transform.childCount;
+        currentMines += children == 0 ? 1 : children;
 
         Instantiate(selected_pattern, new Vector3(xSpawnCoord, ySpawnCoord, 0), new Quaternion());
     }
@@ -22,7 +31,7 @@ public sealed class MineSpawnable : Spawnable<MineSpawnable>
     public override void UpdateConstraints(GameObject obj)
     {
         var children = obj.transform.childCount;
-        CurrentMines -= children == 0 ? 1 : children;
+        currentMines -= children == 0 ? 1 : children;
     }
     
     public override bool IsSpawnable()
